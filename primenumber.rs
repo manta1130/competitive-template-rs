2b183601bba863cc94251b3093962920dca6cf84
manta1130 <46486762+manta1130@users.noreply.github.com>
2020-10-14 19:40:53 +0900

    Update primenumber.rs

--- primenumber.rs
+++ primenumber.rs
@@ -2,20 +2,75 @@ use std::iter::Iterator;
 
 type ValueType = usize;
 
-pub struct PrimeFactorization {
+pub struct Divisor {
     n: ValueType,
     cur: ValueType,
+    flag: bool,
 }
 
-///素因数分解
-impl PrimeFactorization {
-    ///素因数を計算するイテレータを返す。
-    pub fn calc(n: ValueType) -> PrimeFactorization {
-        PrimeFactorization { n, cur: 1 }
+impl Divisor {
+    pub fn calc(n: ValueType) -> Divisor {
+        Divisor {
+            n,
+            cur: 1,
+            flag: false,
+        }
+    }
+}
+impl Iterator for Divisor {
+    type Item = ValueType;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur * self.cur > self.n {
+            None
+        } else if self.flag {
+            if self.cur * self.cur == self.n {
+                return None;
+            }
+            self.flag = false;
+            self.cur += 1;
+            Some(self.n / (self.cur - 1))
+        } else {
+            while self.n % self.cur != 0 {
+                self.cur += 1;
+                if self.cur * self.cur > self.n {
+                    return None;
+                }
+            }
+            self.flag = true;
+            Some(self.cur)
+        }
     }
 }
 
-impl Iterator for PrimeFactorization {
+pub struct PrimeFactorization<'a> {
+    n: ValueType,
+    cur: ValueType,
+    p_list: Option<&'a [ValueType]>,
+    idx: usize,
+}
+
+///素因数分解
+impl<'a> PrimeFactorization<'a> {
+    ///素因数を計算するイテレータを返す。
+    pub fn calc(n: ValueType) -> PrimeFactorization<'a> {
+        PrimeFactorization {
+            n,
+            cur: 1,
+            p_list: None,
+            idx: 0,
+        }
+    }
+    pub fn calc_fast(n: ValueType, p_list: &'a [ValueType]) -> PrimeFactorization<'a> {
+        PrimeFactorization {
+            n,
+            cur: 1,
+            p_list: Some(p_list),
+            idx: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for PrimeFactorization<'a> {
     type Item = ValueType;
     fn next(&mut self) -> Option<Self::Item> {
         loop {
@@ -23,7 +78,15 @@ impl Iterator for PrimeFactorization {
                 return None;
             }
 
-            self.cur += 1;
+            if self.p_list.is_some() {
+                if self.idx >= self.p_list.unwrap().len() {
+                    return None;
+                }
+                self.cur = self.p_list.unwrap()[self.idx];
+                self.idx += 1;
+            } else {
+                self.cur += 1;
+            }
 
             if self.cur * self.cur > self.n {
                 if self.n != 1 {
@@ -34,9 +97,28 @@ impl Iterator for PrimeFactorization {
             }
             if self.n % self.cur == 0 {
                 self.n /= self.cur;
+                if self.p_list.is_some() {
+                    self.idx -= 1;
+                }
                 self.cur -= 1;
                 return Some(self.cur + 1);
             }
         }
     }
+}
+
+pub fn get_primelist(u: usize) -> Vec<usize> {
+    let mut v = vec![true; u + 1];
+    let mut r = vec![];
+    for i in 2..=u {
+        if v[i] {
+            r.push(i);
+            let mut j = i * 2;
+            while j <= u {
+                v[j] = false;
+                j += i;
+            }
+        }
+    }
+    r
 }
